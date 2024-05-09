8c8a9f176ded8fecf91b13c38703de925839cde2
tmddn <wjdtmddn@dsm.hs.kr>
2024-05-09 23:03:17 +0900

    MDN [JS가 뭔가요?]
    2024/05/09

--- JS STUDY/MDN/main.js
+++ JS STUDY/MDN/main.js
@@ -35,3 +35,18 @@ function setUserName() {
 myButton.onclick = function () {
   setUserName();
 };
+
+//javascript 적용하기 연습
+document.addEventListener("DOMContentLoaded", () => {
+  function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+  }
+
+  const buttons = document.querySelectorAll("button");
+
+  for (const button of buttons) {
+    button.addEventListener("click", createParagraph);
+  }
+});
