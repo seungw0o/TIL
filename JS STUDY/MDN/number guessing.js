fc85b2d541b34da4fa580f0ad035742af115cf25
tmddn <wjdtmddn@dsm.hs.kr>
2024-05-09 23:22:30 +0900

    숫자 맞추기 게임
    변수 선언과 랜덤값 정하기
    2024/05/09

---
+++ JS STUDY/MDN/number guessing.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResurt = document.querySelector(".lastResurt");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  alert("I'm a placeholder");
+}
