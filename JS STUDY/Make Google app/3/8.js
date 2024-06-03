b9e221296e2cacef277dcd0d03184b4051510f23
정승우 <ssounsowns@gmail.com>
2024-06-03 23:10:47 +0900

    NOMAD REACT , prettier
    6.0 Introduction
    prettier 고치기 실패
    2024/06/03

--- JS STUDY/Make Google app/3/8.js
+++ JS STUDY/Make Google app/3/8.js
@@ -12,7 +12,7 @@ function handleh1Click() {
   //   h1.classList.add(clickedClass); // clickedClass가 없다면 추가
   // }
   // // 이를 이용하여 원래 남아있던 class 값을 보존가능
-  h1.classList.toggle("clicked"); // 위 코드를 toggle 함수 하나로 끝냄.
-}
+    h1.classList.toggle("clicked");  // 위 코드를 toggle 함수 하나로 끝냄.
+  }
 
 h1.addEventListener("click", handleh1Click);
--- JS STUDY/REACT/NOMAD/5.reactForBeginners/src/Button.js
+++ JS STUDY/REACT/NOMAD/5.reactForBeginners/src/Button.js
@@ -3,7 +3,8 @@ import styles from "./Button.module.css";
 
 
 function Button({text}){
-  return <button className={styles.btn}>{text}</button>;
+  return <button className={styles.btn}
+  >{text}</button>;
 }
 
 Button.propTypes = {
