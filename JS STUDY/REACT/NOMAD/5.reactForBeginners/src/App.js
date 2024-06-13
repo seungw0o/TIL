859cfae5aa9fde572b06743517f07f79400c9b86
정승우 <ssounsowns@gmail.com>
2024-06-13 22:58:02 +0900

    NOMAD REACT
    7.9 Styles
    2024/06/13

--- JS STUDY/REACT/NOMAD/5.reactForBeginners/src/App.js
+++ JS STUDY/REACT/NOMAD/5.reactForBeginners/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import "./styles.css";
 
 function App() {
   return (
