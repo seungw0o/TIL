d3b64a967b551fe4afd4fc72e1daf526ba55621d
정승우 <ssounsowns@gmail.com>
2024-07-28 04:10:46 +0900

    NOMAD TwitterClone
    auth 설정, authStateReady()
    2024/07/28

--- REACT/NOMAD/TwitterClone/src/App.tsx
+++ REACT/NOMAD/TwitterClone/src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
@@ -49,7 +50,7 @@ font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
-    //wait for firebase
+    await auth.authStateReady();
     setLoading(false);
   };
   useEffect(() => {
--- REACT/NOMAD/TwitterClone/src/firebase.ts
+++ REACT/NOMAD/TwitterClone/src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfkGL_yIwUz1NDhq_b2q2h6K-kCqPzNuo",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
