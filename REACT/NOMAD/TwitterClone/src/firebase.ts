be1bbe2894e7f4d1a439ad69e30b6a48d706448e
정승우 <ssounsowns@gmail.com>
2024-07-28 04:03:42 +0900

    NOMAD TwitterClone
    firebase 설치, 초기화
    2024/07/28

---
+++ REACT/NOMAD/TwitterClone/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfkGL_yIwUz1NDhq_b2q2h6K-kCqPzNuo",
+  authDomain: "nwitter-reloaded-nomad.firebaseapp.com",
+  projectId: "nwitter-reloaded-nomad",
+  storageBucket: "nwitter-reloaded-nomad.appspot.com",
+  messagingSenderId: "317866261721",
+  appId: "1:317866261721:web:2bb7fbbec266fdd39152c8",
+};
+
+const app = initializeApp(firebaseConfig);
