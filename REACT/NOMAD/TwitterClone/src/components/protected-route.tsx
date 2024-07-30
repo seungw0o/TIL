faf9ef2eea984178fa739c50d38c18c00ac9dccb
정승우 <ssounsowns@gmail.com>
2024-07-30 13:33:34 +0900

    NOMAD TwitterClone
    #3.3 Protected Routes
    2024/07/30

---
+++ REACT/NOMAD/TwitterClone/src/components/protected-route.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { auth } from "../firebase";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = auth.currentUser;
+  console.log(user);
+
+  if (user === null) {
+    return <Navigate to={"/login"} />;
+  }
+  return children;
+}
