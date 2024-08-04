bc0039e3529386c48a9f11c6b075b9a7c33e5a8b
정승우 <ssounsowns@gmail.com>
2024-08-04 20:50:50 +0900

    NOMAD TwitterClone
    #4.4 Fetching Timeline
    2024/08/04

---
+++ REACT/NOMAD/TwitterClone/src/components/timeline.tsx
@@ -0,0 +1,40 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
+import Tweet from "./tweet";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+export default function Timeline() {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(collection(db, "tweets"));
+    orderBy("createdAt", "desc");
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map(doc => {
+      const { tweet, userId, createdAt, username, photo } = doc.data();
+      return { tweet, userId, createdAt, username, photo, id: doc.id };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return (
+    <Wrapper>
+      {tweets.map(tweet => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
