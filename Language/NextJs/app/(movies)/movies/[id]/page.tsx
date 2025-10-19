a25639dac44aa34ad6ca5c56b7bdc80fb9fe2053
seungW0o <ssounsowns@gmail.com>
2025-10-19 20:09:09 +0900

    params 에러 수정

--- Language/NextJs/app/(movies)/movies/[id]/page.tsx
+++ Language/NextJs/app/(movies)/movies/[id]/page.tsx
@@ -3,17 +3,17 @@ import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MoviesDetail({
-  params: { id },
+  params,
 }: {
   params: { id: string };
 }) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
-        <MovieInfo id={id} />
+        <MovieInfo id={params.id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={id} />
+        <MovieVideos id={params.id} />
       </Suspense>
     </div>
   );
