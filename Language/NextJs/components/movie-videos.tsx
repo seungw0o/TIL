0ad4490221092c60f7d027d5e4fa56ad0c6e3643
seungW0o <ssounsowns@gmail.com>
2025-10-22 16:02:42 +0900

    NOMAD NextJS
    #4.3 Movie Trailers | 2025/10/22

--- Language/NextJs/components/movie-videos.tsx
+++ Language/NextJs/components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../lib/constants";
+import styles from "../styles/movie-video.module.css";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
@@ -10,5 +11,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video: any) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
