8e69a93462a925af13c6abec47277078609828a7
seungW0o <ssounsowns@gmail.com>
2025-10-25 23:26:24 +0900

    빌드 에러

--- Language/NextJs/app/(movies)/movies/[id]/page.tsx
+++ Language/NextJs/app/(movies)/movies/[id]/page.tsx
@@ -3,20 +3,14 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
-
-export const generateMetadata = async ({
-  params,
-}: IParams): Promise<Metadata> => {
+export const generateMetadata = async ({ params }): Promise<Metadata> => {
   const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 };
 
-const MoviesDetailPage = async ({ params }: IParams) => {
+const MoviesDetailPage = ({ params }) => {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
